12e75e23b620551d77224d30db00a88aac755b5d
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2023-03-31 15:05:29 +0300

    Email confirmation

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', views.logoutuser, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-from item.models import Category, Item
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+from item.models import Category, Item, User
 from .forms import SignupForm
+from .tokens import account_activation_token
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user = None
+    
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "You can now log in!")
+        return redirect('/login/')
+    else:
+        messages.error(request, "Activation link is invalid")
+
+    return redirect('/login/')
+
+def activateemail(request, user, to_email):
+    mail_subject = 'Activate your account'
+    message = render_to_string("template_activate_account.html", {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f"{user} check email {to_email} and click the link to activate an account.")
+    else:
+        messages.success(request, f"Problem sending email to {to_email}, check if email is correct.")
 
 def index(request):
     items = Item.objects.filter(is_sold=False).order_by('-created_at')[0:6]
@@ -15,7 +54,10 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            activateemail(request, user, form.cleaned_data.get('email'))
             return redirect('/login/')
     else:
         form = SignupForm()
